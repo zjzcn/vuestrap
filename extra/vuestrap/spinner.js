f461a2cf8b137a67d55ef385713b3556ee38bd27
Kris Zima <kris@mosquito.ie>
2015-09-20 13:38:49 +1000

    add spinner

---
+++ extra/vuestrap/spinner.js
@@ -0,0 +1,32 @@
+require('./spinner.scss');
+module.exports = {
+    template: require('./spinner.html'),
+    replace: true, 
+    data: function(){
+    	return {
+	    	active: false,
+            size: 'md'
+    	};
+    },
+    props: ['id','size','variant','overlay'],
+    methods: {
+    	show: function(){
+    		this.active = true;
+    	},
+    	hide: function(){
+    		this.active = false;
+    	}
+    }, 
+    events: {
+    	'show-spinner': function(id){
+            if (id === this.id){
+                this.show();
+            }
+    	},
+    	'hide-spinner': function(id){
+    		if (id === this.id){
+                this.hide();
+            }
+    	}
+    }
+};
