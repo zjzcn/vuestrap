ff37029f027fdece3070998951312d43e4d6a34c
Kris Zima <kris@mosquito.ie>
2015-09-21 14:46:34 +1000

    change spinner properties to be more specific

--- extra/gritcode/spinner.js
+++ extra/gritcode/spinner.js
@@ -3,30 +3,53 @@ module.exports = {
     template: require('./spinner.html'),
     replace: true, 
     data: function(){
-    	return {
-	    	active: false,
-            size: 'md'
-    	};
+        return {
+            active: false
+        };
     },
-    props: ['id','size','variant','overlay','fixed'],
+    props: {
+        id: {
+            type: String,
+            required: true,
+            default: ''
+        },
+        size: {
+            type: String,
+            default: 'md'
+        },
+        variant: {
+            type: String,
+            default: 'primary'
+        },
+        // if false spinner will be inline
+        overlay: {
+            type: Boolean,
+            default: false
+        },
+        // if fixed spinner-wrapper will have position: fixed set
+        fixed: {
+            type: Boolean,
+            default: false
+        }
+    },
     methods: {
-    	show: function(){
-    		this.active = true;
-    	},
-    	hide: function(){
-    		this.active = false;
-    	}
+        show: function(){
+            this.active = true;
+        },
+        hide: function(){
+            this.active = false;
+        }
     }, 
     events: {
-    	'show-spinner': function(id){
+        'show-spinner': function(id){
             if (id === this.id){
                 this.show();
             }
-    	},
-    	'hide-spinner': function(id){
-    		if (id === this.id){
+        },
+        'hide-spinner': function(id){
+            if (id === this.id){
                 this.hide();
             }
-    	}
+        }
     }
 };
