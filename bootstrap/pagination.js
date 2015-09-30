ce9deab83c0c5981834df8a6e8af88bb0fc7eaec
Kris Zima <kris@mosquito.ie>
2015-09-30 16:42:36 +1000

    add pagination component

--- bootstrap/pagination.js
+++ bootstrap/pagination.js
@@ -1,1 +1,30 @@
-// todo
+require('./_pagination.scss');
+require('./_button-group.scss');
+module.exports = {
+    template: require('./pagination.html'),
+    replace: true, 
+    computed: {
+    	links: function(){
+    		var numberOfPages = Math.ceil(this.totalRows / this.perPage);
+    		return (numberOfPages < 1) ? 1 : numberOfPages;
+    	}
+    },
+    props: {
+      totalRows: {
+        type: Number,
+        default: 20
+      }, 
+      currentPage: {
+        type: Number,
+        default: 1 
+      },
+      perPage: {
+        type: Number,
+        default: 20
+      },
+      variant: {
+        type: String, 
+        default: 'primary'
+      }
+    }
+};
