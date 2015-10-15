e5e9d03be94217de07cad55a228e36076c8ee271
Kris Zima <kris@mosquito.ie>
2015-10-15 12:08:23 +1000

    add listgroup component

--- bootstrap/list-group.js
+++ bootstrap/list-group.js
@@ -1,1 +1,23 @@
-// todo
+require('./_list-group.scss');
+module.exports = {
+    template: require('./list-group.html'),
+    replace: true,
+    // computed: {
+    //     labelVariant: function(){
+    //         return !this.variant || this.variant === 'standard' ? '' : 'label-' + this.variant;
+    //     },
+    //     labelType: function(){
+    //         return !this.type ? '' : 'label-' + this.type;
+    //     }
+    // },
+    // props: {
+    //     variant: {
+    //     	type: String,
+    //     	default: 'primary'
+    //     },
+    //     type: {
+    //         type: String,
+    //         default: ''
+    //     }
+    // }
+};
