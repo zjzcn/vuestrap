c2d40ac90a0598001d8dc16619459d50b8cbec18
Kris Zima <kris@mosquito.ie>
2015-10-15 12:06:01 +1000

    add labels

--- bootstrap/labels.js
+++ bootstrap/labels.js
@@ -1,1 +1,23 @@
-// todo
+require('./_labels.scss');
+module.exports = {
+    template: require('./labels.html'),
+    replace: true,
+    computed: {
+        labelVariant: function(){
+            return !this.variant || this.variant === 'standard' ? '' : 'label-' + this.variant;
+        },
+        labelType: function(){
+            return !this.type ? '' : 'label-' + this.type;
+        }
+    },
+    props: {
+        variant: {
+        	type: String,
+        	default: 'primary'
+        },
+        type: {
+            type: String,
+            default: ''
+        }
+    }
+};
